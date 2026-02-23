b36785a469030f32f354d9bf4c40726d3b772875
Dileep Kesharwani <dileep.kesharwani@cubastion.com>
2026-02-23 10:46:37 +0530

    chnages

--- src/app/shared/utilities/api_endpoints.ts
+++ src/app/shared/utilities/api_endpoints.ts
@@ -1,5 +1,5 @@
-const BASE_URL = "/vip-api";
-//const BASE_URL = "http://localhost:4600";
+//const BASE_URL = "/vip-api";
+const BASE_URL = "http://localhost:4600";
 
 export const API_ENDPOINTS = {
    authenticate:`${BASE_URL}/auth`,
